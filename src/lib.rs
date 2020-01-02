000db1a9c6aa809a04f335184ffab7949c10f576
Awpteamoose <awpteamoose@gmail.com>
2020-01-02 11:23:20 +0000

    initial

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+#![feature(try_blocks)]
+
+mod read_memory;
+
+use std::{
+	ffi::{CStr, CString},
+	os::raw::c_char,
+};
+pub use read_memory::*;
+use anyhow::Context;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct FFIResult<T> {
+	pub value: T,
+	pub success: bool,
+}
+
+#[no_mangle]
+#[allow(clippy::missing_safety_doc)]
+pub unsafe extern "C" fn free_str(ptr: *mut c_char) {
+	let _ = CString::from_raw(ptr);
+}
+
+#[no_mangle]
+#[allow(clippy::missing_safety_doc)]
+#[must_use]
+pub unsafe extern "C" fn read_i32(
+	process_name_raw: *const c_char,
+	dll_raw: *const c_char,
+	offsets_ptr: *const usize,
+	offsets_len: usize,
+) -> FFIResult<i32> {
+	let res: anyhow::Result<_> = try {
+		let process_name = CStr::from_ptr(process_name_raw).to_str()?;
+		let dll = CStr::from_ptr(dll_raw).to_str()?;
+		let offsets = std::slice::from_raw_parts(offsets_ptr, offsets_len);
+		let module = Module::new(process_name, dll)?;
+
+		// TODO: maybe nicer?
+		let (first_offset, tail): (&usize, &[usize]) = offsets.split_first().context("no first offset")?;
+		let (last_offset, middle) = tail.split_last().context("no last offset")?;
+		let mut ptr = read_mem::<u32>(module.pid, first_offset + module.base_address)? as usize;
+		for offset in middle {
+			ptr = read_mem::<u32>(module.pid, offset + ptr)? as usize;
+		}
+
+		read_mem::<i32>(module.pid, last_offset + ptr)?
+	};
+	match res {
+		Ok(value) => FFIResult { success: true, value },
+		_ => FFIResult { success: false, value: 0 },
+	}
+}
+
+#[no_mangle]
+#[allow(clippy::missing_safety_doc)]
+#[must_use]
+pub unsafe extern "C" fn process_path(process_name_raw: *const c_char) -> FFIResult<*mut c_char> {
+	let res: anyhow::Result<_> = try {
+		let process_name = CStr::from_ptr(process_name_raw).to_str()?;
+		let path = read_memory::process_path(process_name)?;
+		CString::new(path)?.into_raw()
+	};
+	match res {
+		Ok(value) => FFIResult { success: true, value },
+		_ => FFIResult { success: false, value: std::ptr::null_mut() },
+	}
+}
---
+++ src/read_memory.rs
@@ -0,0 +1,122 @@
+use scopeguard::defer;
+use winapi::{
+	ctypes::*,
+	um::{handleapi::CloseHandle, tlhelp32::*, winnt::HANDLE},
+};
+
+fn get_pid(process_name: &str) -> anyhow::Result<u32> {
+	let all_processes_snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+	defer!(if unsafe { CloseHandle(all_processes_snapshot) } == 0 {
+		panic!("can't close handle");
+	});
+
+	let mut process = PROCESSENTRY32W::default();
+	{
+		#![allow(clippy::cast_possible_truncation)]
+		process.dwSize = std::mem::size_of::<PROCESSENTRY32W>() as u32;
+	}
+
+	if unsafe { Process32FirstW(all_processes_snapshot, &mut process) } == 0 {
+		return Err(anyhow::anyhow!("can't enumerate processes"));
+	}
+
+	loop {
+		let inspected_process_name = widestring::U16Str::from_slice(&process.szExeFile).to_string_lossy();
+		if process_name == inspected_process_name {
+			break Ok(process.th32ProcessID);
+		}
+
+		if unsafe { Process32NextW(all_processes_snapshot, &mut process) } == 0 {
+			return Err(anyhow::anyhow!("can't find your process"));
+		}
+	}
+}
+
+pub fn process_path(process_name: &str) -> anyhow::Result<String> {
+	let pid = get_pid(process_name)?;
+
+	let process_snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid) };
+	defer!(if unsafe { CloseHandle(process_snapshot) } == 0 {
+		panic!("can't close handle");
+	});
+
+	let mut module_entry = MODULEENTRY32W::default();
+	{
+		#![allow(clippy::cast_possible_truncation)]
+		module_entry.dwSize = std::mem::size_of::<MODULEENTRY32W>() as u32;
+	}
+
+	if unsafe { Module32FirstW(process_snapshot, &mut module_entry) } != 1 {
+		return Err(anyhow::anyhow!("can't enumerate modules"));
+	}
+
+	Ok(widestring::U16Str::from_slice(&module_entry.szExePath).to_string_lossy())
+}
+
+#[derive(Debug)]
+pub struct Module {
+	pub pid: u32,
+	pub base_address: usize,
+	pub base_size: usize,
+	snapshot: HANDLE,
+}
+
+impl Module {
+	pub fn from_pid(pid: u32, dll: &str) -> anyhow::Result<Self> {
+		let process_snapshot: HANDLE = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid) };
+
+		let mut module_entry = MODULEENTRY32W::default();
+		{
+			#![allow(clippy::cast_possible_truncation)]
+			module_entry.dwSize = std::mem::size_of::<MODULEENTRY32W>() as u32;
+		}
+
+		if unsafe { Module32FirstW(process_snapshot, &mut module_entry) } != 1 {
+			return Err(anyhow::anyhow!("can't enumerate modules"));
+		}
+
+		loop {
+			let module_name = widestring::U16Str::from_slice(&module_entry.szModule).to_string_lossy();
+
+			if module_name == dll {
+				return Ok(Self {
+					pid,
+					base_address: module_entry.modBaseAddr as usize,
+					snapshot: process_snapshot,
+					base_size: module_entry.modBaseSize as usize,
+				});
+			}
+
+			if unsafe { Module32NextW(process_snapshot, &mut module_entry) } == 0 {
+				if unsafe { CloseHandle(process_snapshot) } == 0 {
+					panic!("can't close handle");
+				}
+				break Err(anyhow::anyhow!("can't find your module"));
+			}
+		}
+	}
+
+	pub fn new(process_name: &str, dll: &str) -> anyhow::Result<Self> {
+		Self::from_pid(get_pid(process_name)?, dll)
+	}
+}
+
+impl Drop for Module {
+	fn drop(&mut self) {
+		if unsafe { CloseHandle(self.snapshot) } == 0 {
+			panic!("can't close handle");
+		}
+	}
+}
+
+pub fn read_mem<T: Default + Copy + Sized>(pid: u32, address: usize) -> anyhow::Result<T> {
+	let buffer: *mut T = &mut Default::default();
+	unsafe {
+		let success = Toolhelp32ReadProcessMemory(pid, address as *const c_void, buffer as *mut c_void, std::mem::size_of::<T>(), &mut 0);
+		if success == 1 {
+			Ok(*buffer)
+		} else {
+			Err(anyhow::anyhow!("can't read"))
+		}
+	}
+}
